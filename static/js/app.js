ac1595ffad76b6865ab230295c29ca9fd0b6db96
zodiacfireworks <martin.vuelta@gmail.com>
2018-02-22 20:31:04 -0500

    :octocat: First Hatsue Miku Carousel!!

---
+++ static/js/app.js
@@ -0,0 +1,9 @@
+(function ($) {
+    $(document).ready(function () {
+        $('.carousel').carousel({
+            fullWidth: true,
+            padding: 0,
+            indicators: true,
+        });
+    });
+})(jQuery.noConflict());
